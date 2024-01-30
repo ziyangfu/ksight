7a2635f21b284dcfcac72edd838c89da34aad9a3
ziyangfu <fu438484102@163.com>
2024-01-30 15:21:34 +0800

    adjust project layout OK, build OK,prepare to memory tools coding

--- cmake_support/gcc/include/debug.cmake
+++
@@ -1,6 +0,0 @@
-# This is a CMake toolchain include file
-# This file contains the gcc debug build configuration.
-set(CXX_DEBUG_FLAGS "-Wfatal-errors")
-set(C_DEBUG_FLAGS "-Wfatal-errors")
-
-set_optimization_level("0")
--- src/backend/net/HowToDo.md
+++
@@ -1,4 +0,0 @@
-1. PC运行服务端，orin运行客户端
-
-
-获取 tcp_timestamp
