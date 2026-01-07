3c083b73e0d5a9331c7f725d7ae87f602a44d755
ziyangfu <fu438484102@163.com>
2026-01-07 16:24:11 +0800

    removed config json file and add complete shell script

---
+++ third_tools/binary/nettrace/aarch64/config/bash-completion.sh
@@ -0,0 +1,14 @@
+complete -W '-s --saddr -d --daddr --addr -p -D --dport -S --sport -P --port
+             --netns --netns-current --pid --min-latency --pkt-len
+             --tcp-flags --pkt-len --tcp-rtt --tcp--srtt
+
+             --basic --diag --diag-quiet --diag-keep --drop --drop-stack
+             --sock --monitor --rtt --rtt-detail --filter-srtt
+             --filter-minrtt --latency-show --latency-free --latency
+             --latency-summary
+
+             -t --trace --force --ret --detail --date -c --count --hooks
+             --tiny-show --trace-stack --trace-matcher --trace-noclone
+             --func-stats --rate-limit --btf-path
+
+             -v --debug -h --help -V --version' nettrace
